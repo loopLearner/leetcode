6360e51417425f74180359032aff1b0a187fc16c
loopLearner <laksh.challenges@gmail.com>
2025-06-21 15:07:01 +0530

    Time: 8 ms (5.07%), Space: 101.2 MB (5.02%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int maxv = prices[n-1];
+        vector<int> maxis(n);
+        for(int i=n-1;i>=0;i--){
+            maxv=max(prices[i], maxv);
+            maxis[i] = maxv;
+        }
+        int ans = 0;
+        for(int i=0;i<n;i++){
+            ans = max(ans,maxis[i]-prices[i]);
+        }
+        return ans;
+    }
+};
