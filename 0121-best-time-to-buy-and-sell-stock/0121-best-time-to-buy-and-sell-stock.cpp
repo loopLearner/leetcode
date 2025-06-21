0c7bed957b305d1c031dc20cd92cf9e2d6ca6258
loopLearner <laksh.challenges@gmail.com>
2025-06-21 15:08:36 +0530

    Time: 0 ms (100%), Space: 97.3 MB (60.5%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -3,14 +3,18 @@ public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
         int maxv = prices[n-1];
-        vector<int> maxis(n);
+        // vector<int> maxis(n);
+        // for(int i=n-1;i>=0;i--){
+        //     maxv=max(prices[i], maxv);
+        //     maxis[i] = maxv;
+        // }
+        // int ans = 0;
+        // for(int i=0;i<n;i++){
+        //     ans = max(ans,maxis[i]-prices[i]);
+        int ans=0;
         for(int i=n-1;i>=0;i--){
-            maxv=max(prices[i], maxv);
-            maxis[i] = maxv;
-        }
-        int ans = 0;
-        for(int i=0;i<n;i++){
-            ans = max(ans,maxis[i]-prices[i]);
+            maxv = max(maxv, prices[i]);
+            ans = max(ans, maxv - prices[i]);
         }
         return ans;
     }
